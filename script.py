601de7136580233f78ca63945001afe934a6e788
Arthur0Rocha <ar_d_rocha@hotmail.com>
2024-08-27 20:49:09 -0300

    WIP: Using opencv to generate mp4 files to be added to the pptx

--- script.py
+++ script.py
@@ -2,7 +2,7 @@ import os
 import argparse
 
 import pydicom
-import imageio
+import cv2
 from pptx import Presentation
 from pptx.util import Inches
 
@@ -21,6 +21,21 @@ def extract_images_from_dicom(dicom_seq_folder):
     return images
 
 
+def add_video_slide(prs, video_file):
+    slide_layout = prs.slide_layouts[5]  # Blank slide layout
+    slide = prs.slides.add_slide(slide_layout)
+    left = top = Inches(1)
+    slide.shapes.add_movie(
+        video_file,
+        left,
+        top,
+        width=Inches(8.5),
+        height=Inches(6),
+        poster_frame_image=None,
+        mime_type="video/mp4",
+    )
+
+
 def add_image_slide(prs, image_file):
     slide_layout = prs.slide_layouts[5]  # Blank slide layout
     slide = prs.slides.add_slide(slide_layout)
@@ -28,17 +43,20 @@ def add_image_slide(prs, image_file):
     slide.shapes.add_picture(image_file, left, top, width=Inches(8.5), height=Inches(6))
 
 
-def export_to_ppt(images_files, ppt_file):
+def export_to_ppt(input_files, ppt_file):
     prs = Presentation()
-    for image_file in images_files:
-        add_image_slide(prs, image_file)
+    for input_file in input_files:
+        if ".mp4" in input_file:
+            add_video_slide(prs, input_file)
+        else:
+            add_image_slide(prs, input_file)
     prs.save(ppt_file)
 
 
 def setup_parser():
     parser = argparse.ArgumentParser(
         prog="DICOM2PPT",
-        description="Converts multiple DICOM files to images or GIFs and insert them into a PPT file",
+        description="Converts multiple DICOM files to images, GIFs or mp4 videos and insert them into a PPT file",
         epilog="Usage: inputfolder outputfolder",
     )
     parser.add_argument("inputfolder")
@@ -53,7 +71,7 @@ def main():
     dicom_folder = args.inputfolder
     outpath = args.outputfolder
 
-    gif_paths = []
+    media_paths = []
 
     for sequence_folder in os.listdir(dicom_folder):
         print(f"Reading {sequence_folder}")
@@ -62,21 +80,21 @@ def main():
         if not sequence:
             print("Skipping...")
             continue
-        outgifpath = os.path.join(outpath, sequence_folder) + ".gif"
+        format = ".mp4"  # TODO:
+        output_media_path = os.path.join(outpath, sequence_folder) + format
         try:
-            imageio.mimsave(outgifpath, sequence)
+            pass
+            # imageio.mimsave(output_media_path, sequence) # TODO:
         except Exception:
-            imageio.mimsave(
-                outgifpath, [im for im in sequence if im.shape == sequence[0].shape]
-            )
+            # imageio.mimsave(output_media_path, [im for im in sequence if im.shape == sequence[0].shape]) # TODO:
             for i, im in enumerate(sequence):
                 if im.shape != sequence[0].shape:
-                    imageio.imwrite(f"{outgifpath}.{i}.png", im)
+                    # imageio.imwrite(f"{output_media_path}.{i}.png", im)
                     print(f"Bad shape: {i}")
 
-        gif_paths.append(outgifpath)
+        media_paths.append(output_media_path)
 
-    export_to_ppt(gif_paths, os.path.join(outpath, "presentation.pptx"))
+    export_to_ppt(media_paths, os.path.join(outpath, "presentation.pptx"))
 
 
 if __name__ == "__main__":
